9796a5c2ba53624c1fe3eed7b57f6c6985bf024e
Non0reo <nolhan.pieroni@gmail.com>
2021-08-29 22:54:54 +0200

    Index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let headerButtons = document.getElementsByClassName("headerButton");
 let header = document.getElementById("header");
+var coloredHeader;
 
 
 
@@ -10,15 +11,50 @@ function scrollFunction() {
     document.getElementById("header").style.backgroundColor = 'rgb(255, 255, 255)';
     for(let i = 0; i < headerButtons.length; i++){
         headerButtons[i].style.color = 'rgb(27, 32, 39)';
+        /*if(headerButtons[i].is(":hover")){
+          if(coloredHeader == false) headerButtons[i].style.color = 'rgb(255, 255, 255)';
+          else headerButtons[i].style.color = 'rgb(27, 32, 39)';
+        } 
+        else
+        {
+          if(coloredHeader == false) headerButtons[i].style.color = 'rgb(27, 32, 39)';
+          else headerButtons[i].style.color = 'rgb(255, 255, 255)';
+        }*/
+        
     }
+    coloredHeader = true;
   } else {
     document.getElementById("header").style.backgroundColor = 'transparent';
     for(let i = 0; i < headerButtons.length; i++){
         headerButtons[i].style.color = 'rgb(255, 255, 255)';
+        /*if(headerButtons[i].is(":hover")){
+          if(coloredHeader == false) headerButtons[i].style.color = 'rgb(255, 255, 255)';
+          else headerButtons[i].style.color = 'rgb(27, 32, 39)';
+        } 
+        else
+        {
+          if(coloredHeader == false) headerButtons[i].style.color = 'rgb(27, 32, 39)';
+          else headerButtons[i].style.color = 'rgb(255, 255, 255)';
+        }*/
     }
+    coloredHeader = false;
   }
+
+  //console.log(coloredHeader)
 }
 
+
+  for(let i = 0; i < headerButtons.length; i++){
+    if(headerButtons[i].is(":hover")){
+      if(coloredHeader == false) headerButtons[i].style.color = 'rgb(255, 255, 255)';
+      else headerButtons[i].style.color = 'rgb(27, 32, 39)';
+    } 
+    else
+    {
+      if(coloredHeader == false) headerButtons[i].style.color = 'rgb(27, 32, 39)';
+      else headerButtons[i].style.color = 'rgb(255, 255, 255)';
+    }
+  }
 
 
 
