ab234b7bebc30298ff75f99f0a1df2a879851833
Non0reo <nolhan.pieroni@gmail.com>
2021-08-30 19:47:11 +0200

    Added an arrow

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ function scrollFunction() {
     document.getElementById("header").style.backgroundColor = 'rgb(255, 255, 255)';
     for(let i = 0; i < headerButtons.length; i++){
         headerButtons[i].style.color = 'rgb(27, 32, 39)';
+        headerButtons[i].is('::after').style.backgroundColor = 'rgb(27, 32, 39)';
+        //headerButtons[i].style.backgroundColor = 'rgb(27, 32, 39)';
         /*if(headerButtons[i].is(":hover")){
           if(coloredHeader == false) headerButtons[i].style.color = 'rgb(255, 255, 255)';
           else headerButtons[i].style.color = 'rgb(27, 32, 39)';
@@ -27,6 +29,8 @@ function scrollFunction() {
     document.getElementById("header").style.backgroundColor = 'transparent';
     for(let i = 0; i < headerButtons.length; i++){
         headerButtons[i].style.color = 'rgb(255, 255, 255)';
+        headerButtons[i].is('::after').style.backgroundColor = 'rgb(255, 255, 255)';
+        //headerButtons[i].style.backgroundColor = 'rgb(255, 255, 255)';
         /*if(headerButtons[i].is(":hover")){
           if(coloredHeader == false) headerButtons[i].style.color = 'rgb(255, 255, 255)';
           else headerButtons[i].style.color = 'rgb(27, 32, 39)';
